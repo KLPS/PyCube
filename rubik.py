5ab985443010dd1e44903d03620b2da8414c8bd1
Kalizox <herizoutheo.godefroy@gmail.com>
2019-07-09 20:10:01 +0200

    Update and rename main.py to rubik.py

---
+++ rubik.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.7
+# main.py
+
+# Donc on part sur quel types de liste?
+# 1. La solution optimisée, liste des arrêtes, des bords et des centres
+# 2. Liste par ligne/colonne ?
+
+class rubik_face:
+  '''need comment
+  '''
+  def __init__(self):
+    m_position = []
+    m_color = [] # from 0 to 5
+  def mix(nb):
+    '''to do
+    '''
+  def rotation(pos[], direction):
+    '''to do
+    '''
+  
